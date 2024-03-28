a4ce8ef27aa0a56b9c03f8b75e1a61bed93ec305
jwzhou1 <zhou.jiaw@northeastern.edu>
2024-03-28 11:07:27 -0700

    user profile image can be uploaded from local library

--- components/ImageManager.js
+++ components/ImageManager.js
@@ -1,11 +1,11 @@
 import { View, StyleSheet, Button, Alert, Image } from "react-native";
 import React, { useState } from "react";
-
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImageManager({ receiveImageURI }) {
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
   const [imageUri, setImageUri] = useState("");
+
   async function verifyPermission() {
     if (status.granted) {
       return true;
@@ -15,8 +15,10 @@ export default function ImageManager({ receiveImageURI }) {
       return permissionResponse.granted;
     } catch (err) {
       console.log(err);
+      return false;
     }
   }
+
   async function takeImageHandler() {
     try {
       const havePermission = await verifyPermission();
@@ -25,18 +27,45 @@ export default function ImageManager({ receiveImageURI }) {
         return;
       }
 
-      const results = await ImagePicker.launchCameraAsync({
+      const result = await ImagePicker.launchCameraAsync({
         allowsEditing: true,
       });
-      receiveImageURI(results.assets[0].uri);
-      setImageUri(results.assets[0].uri);
+
+      if (!result.canceled) {
+        receiveImageURI(result.assets[0].uri);
+        setImageUri(result.assets[0].uri);
+      }
     } catch (err) {
       console.log(err);
     }
   }
+
+  async function pickImageHandler() {
+    try {
+      const havePermission = await verifyPermission();
+      if (!havePermission) {
+        Alert.alert("You need to give permission");
+        return;
+      }
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      });
+
+      if (!result.canceled) {
+        receiveImageURI(result.assets[0].uri);
+        setImageUri(result.assets[0].uri);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Button title="Take an Image" onPress={takeImageHandler} />
+      <Button title="Upload from Library" onPress={pickImageHandler} />
       {imageUri && (
         <Image
           style={styles.image}
@@ -48,9 +77,13 @@ export default function ImageManager({ receiveImageURI }) {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     marginVertical: 5,
   },
-  image: { width: 100, height: 100 },
+  image: {
+    width: 100,
+    height: 100,
+  },
 });
